1cdabdbbe72ff98860c9429f91320bcecff96b63
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-28 19:55:21 +0530

    Auto Push

--- elastic-cache.tf
+++ elastic-cache.tf
@@ -21,7 +21,7 @@ resource "aws_elasticache_parameter_group" "default" {
 # Create Subnet Group
 
 resource "aws_elasticache_subnet_group" "redis_subnet_group" {
-  name       = "robot-${var.ENV}-redis-subnet_group"
+  name       = "robot-${var.ENV}-redis-subnet-group"
   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
 }
 
