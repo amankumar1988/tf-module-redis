697243f32acbdf57e575d7b4ab11aa3410a1b291
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-28 23:12:43 +0530

    Auto Push

--- vars.tf
+++ vars.tf
@@ -1,1 +1,5 @@
 variable "ENV" {}
+variable "ELASTIC_CACHE_PORT" {}
+variable "ELASTIC_CACHE_INSTANCE_TYPE" {}
+variable "ELASTIC_CACHE_NODE_COUNT " {}
+variabdata "ELASTIC_CACHE_ENGINE_VERSION" {} 
