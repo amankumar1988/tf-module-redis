18b73ae69835e66c6a39b1108d3af15dba34d95d
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-28 22:36:24 +0530

    Auto Push

--- r53.tf
+++ r53.tf
@@ -1,9 +1,13 @@
 # This creates CNAME record for mongodb
 
-resource "aws_route53_record" "redis" {
-  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
-  name    = "redis-${var.ENV}"
-  type    = "CNAME"
-  ttl     = 10
-  records = [aws_elasticache_cluster.redis]
+# resource "aws_route53_record" "redis" {
+#   zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
+#   name    = "redis-${var.ENV}"
+#   type    = "CNAME"
+#   ttl     = 10
+#   records = [aws_elasticache_cluster.redis]
+# }
+
+output "redis" {
+    value = aws_elasticache_cluster.redis
 }
