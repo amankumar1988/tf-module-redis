0a6dd44ca68dbca984f2ef248dcdd18c18c19df1
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-28 19:10:15 +0530

    Auto Push

--- elastic-cache.tf
+++ elastic-cache.tf
@@ -4,7 +4,7 @@ resource "aws_elasticache_cluster" "redis" {
   node_type            = "cache.t3.small"
   num_cache_nodes      = 1
   parameter_group_name = aws_elasticache_parameter_group.default.name
-  engine_version       = "6.2"
+  engine_version       = "6.x"
   port                 = 6379
   security_group_ids   = [aws_security_group.allow_redis.id]
   subnet_group_name    = aws_elasticache_parameter_group.redis_subnet_group
@@ -14,7 +14,7 @@ resource "aws_elasticache_cluster" "redis" {
 
 resource "aws_elasticache_parameter_group" "default" {
   name    = "robot-${var.ENV}-redis-pg"
-  family  = "redis6.2"
+  family  = "redis6.x"
 }
 
 
