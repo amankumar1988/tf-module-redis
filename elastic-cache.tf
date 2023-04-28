bd855c0a3f74f060988092c6a4b13e63ed28b9e2
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-28 19:09:50 +0530

    Auto Push

--- elastic-cache.tf
+++ elastic-cache.tf
@@ -7,6 +7,7 @@ resource "aws_elasticache_cluster" "redis" {
   engine_version       = "6.2"
   port                 = 6379
   security_group_ids   = [aws_security_group.allow_redis.id]
+  subnet_group_name    = aws_elasticache_parameter_group.redis_subnet_group
 }
 
 # Creates Parameter Group
