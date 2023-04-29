39940f6b754a085d81a63e2c8063c85bd1d2990b
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-29 21:38:18 +0530

    Auto Push

--- elastic-cache.tf
+++ elastic-cache.tf
@@ -1,27 +1,23 @@
 resource "aws_elasticache_cluster" "redis" {
   cluster_id           = "robot-${var.ENV}-redis"
   engine               = "redis"
-  node_type            = var.ELASTIC_CACHE_INSTANCE_TYPE
+  node_type            = var.ELASTIC_CACHE_NODE_TYPE
   num_cache_nodes      = var.ELASTIC_CACHE_NODE_COUNT
   parameter_group_name = aws_elasticache_parameter_group.default.name
-  engine_version       = "6.x"
+  engine_version       = var.ELASTIC_CACHE_ENGINE_VERSION
   port                 = var.ELASTIC_CACHE_PORT
   security_group_ids   = [aws_security_group.allow_redis.id]
   subnet_group_name    = aws_elasticache_subnet_group.redis_subnet_group.name
 }
 
-# Creates Parameter Group
-
+# Creates Parameter group
 resource "aws_elasticache_parameter_group" "default" {
-  name    = "robot-${var.ENV}-redis-pg"
-  family  = "redis6.x"
+  name                 = "robot-${var.ENV}-redis-pg"
+  family               = "redis6.x"
 }
 
-
-# Create Subnet Group
-
+# Creates Subnet Group.
 resource "aws_elasticache_subnet_group" "redis_subnet_group" {
-  name       = "robot-${var.ENV}-redis-subnet-group"
-  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+  name                 = "robot-${var.ENV}-redis-subet-group"
+  subnet_ids           = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
 }
-
