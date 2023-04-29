3f8f02032c643253f2d546b71e8b083281c8ea64
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-29 22:43:03 +0530

    Auto Push

--- r53.tf
+++ r53.tf
@@ -5,7 +5,7 @@ resource "aws_route53_record" "redis" {
   name    = "redis-${var.ENV}"
   type    = "CNAME"
   ttl     = 10
-  records = [aws_elasticache_cluster.redis.num_cache_nodes[0].address]
+  records = [aws_elasticache_cluster.redis.cache_nodes[0].address]
 }
 
 # output "redis" {
